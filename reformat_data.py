290a1bf485d7a5f66911bb91d8253c70a3bb9106
acidrobin <jclayton558@gmail.com>
2023-11-11 14:35:24 +0000

    generated tvt sets

--- reformat_data.py
+++ reformat_data.py
@@ -38,25 +38,65 @@ def convert_summaries_to_string(thread):
 def generate_end_to_end():
 
     thread_names = [name for name in os.listdir(op.join("debatabase_data","threads")) if name.endswith(".csv")]
+    topics_df = pd.read_csv("debatabase_data/topics_df.csv")
 
-    cs = []
-    ss = []
+    # topics_df["topic"] = topics_df["topic"].apply(lambda x: x.strip("."))
+
+    test_names = topics_df[topics_df["set"] == "test"]["topic"].to_list()
+    train_names = topics_df[topics_df["set"] == "train"]["topic"].to_list()
+    val_names = topics_df[topics_df["set"] == "val"]["topic"].to_list()
+
+
+
+    cs_train = []
+    ss_train = []
+
+    cs_val = []
+    ss_val = []
+
+    cs_test = []
+    ss_test = []
 
     for name in thread_names:
 
         thread_csv = pd.read_csv(op.join("debatabase_data","threads",name))
+        #Comments are shuffled so that the llm cannot learn the simple pattern in the
+        #comments (alternating attack and support)
         csv_shuffled = thread_csv.sample(frac=1, random_state=42)
         new = csv_shuffled[csv_shuffled["comment_id"].str.startswith("p")]
+        
+
         comments_as_string = convert_comments_to_string(new)
         summaries_as_string = convert_summaries_to_string(new)
 
-        cs.append(comments_as_string)
-        ss.append(summaries_as_string)
+        name = name[:-4]
+        print(name)
 
+        if name in test_names:
+            cs_test.append(comments_as_string)
+            ss_test.append(summaries_as_string)
+        elif name in val_names:
+            cs_val.append(comments_as_string)
+            ss_val.append(summaries_as_string)
+        elif name in train_names:
+            cs_train.append(comments_as_string)
+            ss_train.append(summaries_as_string)
+        else:
+            raise ValueError("The name should be either in train, test or val")
+            
 
-    summaries_df = pd.DataFrame({"comments": cs,
-                                 "summaries": ss})
+    summaries_df_train = pd.DataFrame({"comments": cs_train,
+                                    "summaries": ss_train})
     
-    summaries_df.to_csv("debatabase_data/end_to_end.csv")
+    summaries_df_val = pd.DataFrame({"comments": cs_val,
+                                    "summaries": ss_val})
+    
+    summaries_df_test = pd.DataFrame({"comments": cs_test,
+                                    "summaries": ss_test})
+    
+    summaries_df_train.to_csv("debatabase_data/end_to_end_train.csv")
+    summaries_df_val.to_csv("debatabase_data/end_to_end_val.csv")
+    summaries_df_test.to_csv("debatabase_data/end_to_end_test.csv")
+
 
 generate_end_to_end()
