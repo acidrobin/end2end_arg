fcfa3fd42dbe8e823b891ebd95ca910f4f759f61
acidrobin <jclayton558@gmail.com>
2024-01-15 19:21:59 +0000

    fixed argparse error

--- bloom_model_test.py
+++ bloom_model_test.py
@@ -2,9 +2,10 @@
 from datasets import Dataset
 from peft import LoraConfig, PeftModel
 import transformers
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, TrainingArguments, TrainerCallback,  GenerationConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, TrainingArguments, TrainerCallback, Seq2SeqTrainer, GenerationConfig
 from trl import SFTTrainer
 import torch
+from math import inf
 import pandas as pd
 
 from preproc_utils import get_preprocessed_debatabase_sft
@@ -12,37 +13,10 @@ from preproc_utils import get_preprocessed_debatabase_sft
 from datasets import load_metric
 
 from copy import deepcopy
-from summary_metrics import compute_node_stance_acc_f1_ged
-import re
-import os
-import os.path as op
-
-from argparse import ArgumentParser
-
-parser = ArgumentParser()
-
-parser.add_argument("--lora_dropout", type=float, default=0.5)
-parser.add_argument("--learning_rate",type=float,default=1e-4)
-parser.add_argument("--weight_decay",type=float, default=0.001)
-args = parser.parse_args()
-
-dir_name = "_".join([str(k) + "_" + str(v) for k,v in vars(args).items()])
-
-
-
-MULTILEVEL=False
-
-if MULTILEVEL==True:
-    scores_dir = op.join("scores", dir_name)
-else:
-    scores_dir = op.join("scores_multilevel", dir_name)
-
-if not op.exists(scores_dir):
-    os.mkdir(scores_dir)
-
 
 meteor = load_metric('meteor')
 rouge = load_metric('rouge')
+
 
 
 def compute_metrics(predictions, references):
@@ -51,16 +25,13 @@ def compute_metrics(predictions, references):
     rouge_output = rouge.compute(
          predictions=predictions, references=references, rouge_types=['rouge2'])['rouge2'].mid
 
-    node_acc, node_f1, ged = compute_node_stance_acc_f1_ged(predictions=predictions, references=references)
-
     return {
         'meteor_score': round(meteor_output['meteor'], 4),
         'rouge2_precision': round(rouge_output.precision, 4),
         'rouge2_recall': round(rouge_output.recall, 4),
         'rouge2_f_measure': round(rouge_output.fmeasure, 4),
-        'node stance f1': round(node_f1, 4),
-        'node stance acc': round(node_acc, 4),
-        "graph edit distance": round(ged, 4)
+        # 'node stance f1': round(node_f1, 4),
+        # 'node stance acc': round(node_acc, 4)
     }
 
 
@@ -68,7 +39,7 @@ def compute_metrics(predictions, references):
 
 class EvalCallback(TrainerCallback):
     def __init__(self):
-        self.best_rouge = -1
+        self.best_rouge = -inf
         self.best_epoch = 0
         self.scores = []
         self.sample_outputs = []
@@ -77,15 +48,19 @@ class EvalCallback(TrainerCallback):
     def on_epoch_begin(self, *args, **kwargs):
 
 
+        # model2 = deepcopy(model)
         model.eval()
+        # model2.bfloat16()
 
         gold_texts = []
         generated_texts = []
 
         generation_config=GenerationConfig(
             do_sample=False,
-            max_new_tokens=512,        
+            max_new_tokens=512,
+        
         )
+
 
         for sample in val_dataset:
             gold_texts.append(sample["output"])
@@ -95,31 +70,22 @@ class EvalCallback(TrainerCallback):
 
             output_tok = model.generate(input_ids=input_tok, generation_config=generation_config)
             generated_text = tokenizer.decode(output_tok[0])
-            output_text = re.split("\[EOG\]|\[/INST\]",generated_text)[1]
+            output_text = generated_text.split("[/INST]</s>", 1)[1]
             generated_texts.append(output_text)
         
         # del(model2)
 
         metrics = compute_metrics(predictions=generated_texts, references=gold_texts)
-  
-  
         if metrics["rouge2_f_measure"] > self.best_rouge:
             trainer.model.save_pretrained("saved_model")
         metrics["epoch"] = len(self.scores) +1
         self.scores.append(metrics)
         scores_df = pd.DataFrame(self.scores)
+        scores_df.to_csv("scores/llama_results.csv")
 
-        self.sample_outputs.append(generated_texts[-1])
+        self.sample_outputs.append(generated_texts[0])
 
-
-        # if MULTILEVEL:
-        #     folder_name = "scores_multilevel"
-        # else:
-        #     folder_name = "scores"
-
-        scores_df.to_csv(f"{scores_dir}/llama_results.csv")
-
-        with open(f"{scores_dir}/sample_output.txt","w") as sample_file:
+        with open("scores/sample_output.txt","w") as sample_file:
             for i, text in enumerate(self.sample_outputs):
                 sample_file.write(f"epoch {i+1}\n")
                 sample_file.write(text + "\n\n")
@@ -128,12 +94,12 @@ class EvalCallback(TrainerCallback):
 
 eval_callback = EvalCallback()
 
+train_dataset = get_preprocessed_debatabase_sft("train")
 
-train_dataset = get_preprocessed_debatabase_sft("train",multilevel=MULTILEVEL)
-val_dataset = get_preprocessed_debatabase_sft("val",multilevel=MULTILEVEL)
- 
+val_dataset = get_preprocessed_debatabase_sft("val")
+
 # train_dataset = train_dataset.select(range(2))
-# val_dataset = val_dataset.select(range(1))
+# val_dataset = val_dataset.select(range(2))
 
 
 
@@ -147,12 +113,8 @@ bnb_config = BitsAndBytesConfig(
 
 # LoRa
 peft_config = LoraConfig(
-    # r=8,
-    # lora_alpha=32,
-    # lora_dropout=0.05,
-
     lora_alpha=16,
-    lora_dropout=args.lora_dropout,
+    lora_dropout=0.1,
     r=64,
     bias='none',
     task_type='CAUSAL_LM',
@@ -168,14 +130,14 @@ peft_config = LoraConfig(
 # Llama 2
 
 model = AutoModelForCausalLM.from_pretrained(
-    'meta-llama/Llama-2-7b-hf',
-    quantization_config=bnb_config,
+    'bigscience/bloom-1b1',
+    # quantization_config=bnb_config,
 )
 # model.cuda()
 model.config.use_cache = False
 model.config.pretraining_tp = 1
 
-tokenizer = AutoTokenizer.from_pretrained('meta-llama/Llama-2-7b-hf', trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained('bigscience/bloom-1b1', trust_remote_code=True)
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = 'right'
 
@@ -192,15 +154,15 @@ training_arguments = TrainingArguments(
     per_device_eval_batch_size=1,
     gradient_accumulation_steps=1,
     evaluation_strategy='epoch',
-    optim='adamw_torch_fused',
+    optim='paged_adamw_32bit',
     # save_steps=10000,
     save_strategy="epoch",
     # resume_from_checkpoint=False,
     logging_steps=10,
     # eval_steps=100,
     # save_steps=100,
-    learning_rate=args.learning_rate,
-    weight_decay=args.weight_decay, #0.001
+    learning_rate=1e-3,
+    # weight_decay=0.001,
     fp16=True,
     bf16=False,
     max_grad_norm=0.3,
@@ -215,7 +177,7 @@ trainer = SFTTrainer(
     model=model,
     train_dataset=train_dataset,
     eval_dataset=val_dataset,
-    peft_config=peft_config,
+    # peft_config=peft_config,
     dataset_text_field='text',
     max_seq_length=7000,
     tokenizer=tokenizer,
@@ -226,5 +188,27 @@ trainer = SFTTrainer(
 
 
 trainer.train()
-trainer.model.save_pretrained('llama-2-7b-nmt')
+# trainer.model.save_pretrained('llama-2-7b-nmt')
 
+
+# ## Inference
+
+# In[ ]:
+
+
+# Checkpoint
+
+# base = AutoModelForCausalLM.from_pretrained(
+#     'meta-llama/Llama-2-7b-hf',
+#     low_cpu_mem_usage=True,
+#     return_dict=True,
+#     torch_dtype=torch.float16,
+#     device_map={"": 0}
+# )
+# model = PeftModel.from_pretrained(base, 'llama-2-7b-nmt')
+# model = model.merge_and_unload()
+
+# tokenizer = AutoTokenizer.from_pretrained('meta-llama/Llama-2-7b-hf', trust_remote_code=True)
+# tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+# tokenizer.pad_token = tokenizer.eos_token
+# tokenizer.padding_side = 'right'
--- run_llama_sum.sh
+++ run_llama_sum.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 source ~/.bashrc
 conda activate end2end_arg
-python llama_summ.py lora_dropout 0.1 learning_rate 1e-3 weight_decay 0.001
+python llama_summ.py --lora_dropout 0.1 --learning_rate 1e-3 --weight_decay 0.001
